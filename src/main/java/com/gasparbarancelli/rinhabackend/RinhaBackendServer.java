3fc07bd5dc75997f7d4f6b5848d4d733a53a57b6
gasparbarancelli <gasparbarancelli@gmail.com>
2024-02-17 10:23:12 -0300

    imp nativa com procedure

--- src/main/java/com/gasparbarancelli/rinhabackend/RinhaBackendServer.java
+++ src/main/java/com/gasparbarancelli/rinhabackend/RinhaBackendServer.java
@@ -1,71 +1,46 @@
 package com.gasparbarancelli.rinhabackend;
 
-import io.javalin.Javalin;
+import com.sun.net.httpserver.HttpServer;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.concurrent.Executors;
 import java.util.logging.Logger;
 
 public class RinhaBackendServer {
 
     private static final Logger LOGGER = Logger.getLogger(RinhaBackendServer.class.getName());
 
-    private final DataSource dataSource = new DataSource();
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         LOGGER.info("Iniciando aplicacao");
         new RinhaBackendServer().startServer();
     }
 
-    public void startServer() {
-        var portNumber = getPortNumber();
-        if (portNumber.isEmpty()) {
+    public void startServer() throws IOException {
+        var socketAddress = getSocketAddress();
+        if (socketAddress.isEmpty()) {
             LOGGER.info("Defina a variavel de ambiente HTTP_PORT para iniciar o servidor web");
             return;
         }
 
-        Javalin.create(config -> {
-            config.useVirtualThreads = true;
-            config.http.asyncTimeout = 10_000L;
-        }).get("/clientes/{clienteId}/extrato", ctx -> {
-            var clienteId = Integer.parseInt(ctx.pathParam("clienteId"));
-            if (Cliente.naoExiste(clienteId)) {
-                ctx.status(404);
-                return;
-            }
-            var extrato = dataSource.extrato(clienteId);
-            var json = TransacaoMapper.map(extrato);
-            ctx.result(json);
-            ctx.status(200);
-            ctx.contentType("application/json");
-        }).post("/clientes/{clienteId}/transacoes", ctx -> {
-            try {
-                var clienteId = Integer.parseInt(ctx.pathParam("clienteId"));
-                if (Cliente.naoExiste(clienteId)) {
-                    ctx.status(404);
-                    return;
-                }
-                var body = ctx.body();
-                var transacaoRequisicao = TransacaoMapper.map(body);
-                var transacaoResposta = dataSource.insert(transacaoRequisicao.geraTransacao(clienteId));
-
-                var json = TransacaoMapper.map(transacaoResposta);
-                ctx.result(json);
-                ctx.status(200);
-                ctx.contentType("application/json");
-            } catch (Exception e) {
-                ctx.status(422);
-            }
-        }).start(portNumber.get());
+        var httpServer = HttpServer.create(socketAddress.get(), 0);
+        httpServer.createContext("/clientes", new TransacaoHttpHandler());
+        httpServer.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+        httpServer.start();
     }
 
-    private Optional<Integer> getPortNumber() {
+    private Optional<InetSocketAddress> getSocketAddress() {
         var port = System.getenv("HTTP_PORT");
         if (Objects.isNull(port)) {
-            //return Optional.empty();
-            port = "8080";
+            return Optional.empty();
         }
-        return Optional.of(Integer.parseInt(port));
+
+        LOGGER.info("Servidor http respondendo na porta " + port);
+
+        var socketAddress = new InetSocketAddress(Integer.parseInt(port));
+        return Optional.of(socketAddress);
     }
 
 }
---
+++ src/main/java/com/gasparbarancelli/rinhabackend/TransacaoHttpHandler.java
@@ -0,0 +1,66 @@
+package com.gasparbarancelli.rinhabackend;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+
+final class TransacaoHttpHandler implements HttpHandler {
+
+    private final DataSource dataSource = new DataSource();
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        var customExchange = new CustomHttpExchange(exchange);
+
+        var path = customExchange.getPath();
+
+        var split = path.split("/");
+        var id = Integer.parseInt(split[2]);
+
+        if (Cliente.naoExiste(id)) {
+            exchange.sendResponseHeaders(404, 0);
+            exchange.close();
+            return;
+        }
+
+        if (customExchange.isPost()) {
+            doPost(customExchange, id);
+            return;
+        }
+
+        doGet(customExchange, id);
+    }
+
+    private void doPost(CustomHttpExchange exchange, int clienteId) {
+        try {
+            var body = exchange.getBody();
+            var transacaoRequisicao = TransacaoMapper.map(body);
+            var transacaoResposta = dataSource.insert(transacaoRequisicao.geraTransacao(clienteId));
+
+            var json = TransacaoMapper.map(transacaoResposta);
+
+            exchange.addHeader("Content-Type", "application/json");
+            exchange.sendResponseHeaders(200, json.length());
+            exchange.setBody(json);
+        } catch (Exception e) {
+            exchange.sendResponseHeaders(422, 0);
+        } finally {
+            exchange.close();
+        }
+    }
+
+    private void doGet(CustomHttpExchange exchange, int clienteId) {
+        try {
+            var extrato = dataSource.extrato(clienteId);
+            var json = TransacaoMapper.map(extrato);
+
+            exchange.addHeader("Content-Type", "application/json");
+            exchange.sendResponseHeaders(200, json.length());
+            exchange.setBody(json);
+        } finally {
+            exchange.close();
+        }
+    }
+
+}
